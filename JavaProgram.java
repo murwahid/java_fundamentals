faab68c6e196175e5255215fc76928749f2ad373
Mustafa Wahid <mustafarwahid@gmail.com>
2022-04-04 11:49:42 -0400

    - initial commit

---
+++ JavaProgram.java
@@ -0,0 +1,7 @@
+public class JavaProgram {
+    public static void main(String [] args) {
+        System.out.println("name: Mustafa Wahid"); 
+        System.out.println("I am Way too old"); 
+        System.out.println("My hometown is New York,NY");
+    }
+}
